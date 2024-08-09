767d9851d0d4dcfd1009d1c5c98d842143e9a962
Your Name <you@example.com>
2024-08-08 23:05:59 -0500

    metodo  post

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const API_URL_RANDOM = ' https://api.thecatapi.com/v1/images/search?limit=2&api_key=live_y6ugbp5HOwshYFSac1hCZnsD2gbmPWZM7vIN2a316cBIHQt2YjP8twwH5bntTBO7';
 
-const API_URL_FAVOURITES = ' https://api.thecatapi.com/v1/favourites?limit=2&api_key=live_y6ugbp5HOwshYFSac1hCZnsD2gbmPWZM7vIN2a316cBIHQt2YjP8twwH5bntTBO7';
+const API_URL_FAVOURITES = ' https://api.thecatapi.com/v1/favourites?api_key=live_y6ugbp5HOwshYFSac1hCZnsD2gbmPWZM7vIN2a316cBIHQt2YjP8twwH5bntTBO7';
 
 const spanError = document.getElementById('Error')
 
@@ -22,16 +22,31 @@ async function loadRandomMichis() {
   }
 
 async function loadFavouritesMichis() {
-    const response = await fetch(AP); // Esperando respuesta de la API
+    const response = await fetch(API_URL_RANDOM); // Esperando respuesta de la API
     const data = await response.json(); // la respuese convierta en JSON
     console.log(data)
 
     if (response.status !== 200) {
-      spanError.innerHTML = " Hubo un error:" + response.status;
+      spanError.innerHTML = " Hubo un error:" + response.status +  data.message;
   }
 }
+
+async function saveFavouriteMichis() {
+  const response = await fetch(API_URL_FAVOURITES, {
+    method:'POST',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify({
+      image_id: 'dje'
+    }),
+  });
+  console.log('save')
+  console.log(response)
+}
+
 // Asignacion de la función al evento click del botón
-const reloadButton = document.querySelector('.recargar');
+const reloadButton = document.querySelector('.recargar', 'btn1', 'btn2');
 reloadButton.addEventListener('click',  loadRandomMichis);
 
 // Cargar una imagen inicial
