3d503b0726faefa1bc7d773813b46acce6f0d481
Your Name <you@example.com>
2024-08-08 21:22:41 -0500

    Maquetacion del proyecto

--- main.js
+++ main.js
@@ -1,28 +1,39 @@
-const URL = ' https://api.thecatapi.com/v1/images/search?limit=10';
-/*
-fetch(URL)
-.then(res => res.json())
-.then(data => {
-    const img = document.querySelector('img');
-    img.src =  data[0].url;
-});
- */
+const API_URL_RANDOM = ' https://api.thecatapi.com/v1/images/search?limit=2&api_key=live_y6ugbp5HOwshYFSac1hCZnsD2gbmPWZM7vIN2a316cBIHQt2YjP8twwH5bntTBO7';
 
-async function CatImage () {
-  try {
-    const response =  fetch(URL); // Esperando respuesta de la API
-    const data = await response.json(); // la respuesta se convierta en JSON
-    const img = document.querySelector('img');
-    img.src = data[0].url; // Asignacion de la URL de la imagen al atributo src
-  } catch (error) {
-    console.error('Error al cargar la imagen:', error); // Maneja cualquier error que ocurra
+const API_URL_FAVOURITES = ' https://api.thecatapi.com/v1/favourites?limit=2&api_key=live_y6ugbp5HOwshYFSac1hCZnsD2gbmPWZM7vIN2a316cBIHQt2YjP8twwH5bntTBO7';
+
+const spanError = document.getElementById('Error')
+
+async function loadRandomMichis() {
+    const response = await fetch(API_URL_RANDOM); // Esperando respuesta de la API
+    const data = await response.json(); // la respuest se convierta en JSON
+    console.log('Random')
+    console.log(data)
+
+    if (response.status !== 200) {
+       spanError.innerHTML = " Hubo un error:" + response.status;
+    } else {
+      const imagen1 = document.getElementById('imagen1');
+      const imagen2 = document.getElementById('imagen2');
+
+      imagen1.src = data[0].url;
+      imagen2.src = data[1].url;
+  }
+  }
+
+async function loadFavouritesMichis() {
+    const response = await fetch(AP); // Esperando respuesta de la API
+    const data = await response.json(); // la respuese convierta en JSON
+    console.log(data)
+
+    if (response.status !== 200) {
+      spanError.innerHTML = " Hubo un error:" + response.status;
   }
 }
-
 // Asignacion de la función al evento click del botón
 const reloadButton = document.querySelector('.recargar');
-reloadButton.addEventListener('click', CatImage);
+reloadButton.addEventListener('click',  loadRandomMichis);
 
 // Cargar una imagen inicial
-CatImage();
-
+loadRandomMichis();
+loadFavouritesMichis();
